55abc063339f5cc97263cea02773ef189f47c4f2
jjdredd <the.guard@mail.ru>
2020-01-29 14:39:28 -0700

    finished tfrecord script

--- create_trecord.py
+++ create_trecord.py
@@ -6,28 +6,37 @@ import glob
 import tensorflow as tf
 from object_detection.utils import dataset_util
 
+import os
 import io
 from PIL import Image
 
 
+annotations_dir = 'prepare_data'
+image_dir = 'prepare_data'
+output_dir = 'prepare_data'
+output_path = os.path.join(output_dir, 'train.tfrecord')
+
 def create_tfrecord(data_dict):
     """creating tfrecord  from a dict with all needed data"""
-    with tf.io.gfile.GFile(data_dict['image'], 'rb') as fid:
+    with tf.io.gfile.GFile(os.path.join(image_dir, data_dict['image']), 'rb') as fid:
         encoded_jpg = fid.read()
     encoded_jpg_io = io.BytesIO(encoded_jpg)
     image = Image.open(encoded_jpg_io)
     width, height = image.size
 
-    filename = data_dict['image']
+    filename = data_dict['image'].encode('utf8')
     image_format = b'png'
 
-    for i in len(data_dict['names']):
+    xmins = []
+    xmaxs = []
+    ymins = []
+    ymaxs = []
+
+    for i in range(len(data_dict['names'])):
         xmins.append(data_dict['xmins'][i] / width)
         xmaxs.append(data_dict['xmaxs'][i] / width)
-        ymins.append(row['ymin'] / height)
-        ymaxs.append(row['ymax'] / height)
-        classes_text.append(row['class'].encode('utf8'))
-        classes.append(class_text_to_int(row['class']))
+        ymins.append(data_dict['ymins'][i] / height)
+        ymaxs.append(data_dict['ymaxs'][i] / height)
 
     tf_example = tf.train.Example(features=tf.train.Features(feature={
         'image/height': dataset_util.int64_feature(height),
@@ -40,8 +49,8 @@ def create_tfrecord(data_dict):
         'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
         'image/object/bbox/ymin': dataset_util.float_list_feature(ymins),
         'image/object/bbox/ymax': dataset_util.float_list_feature(ymaxs),
-        'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
-        'image/object/class/label': dataset_util.int64_list_feature(classes),
+        'image/object/class/text': dataset_util.bytes_list_feature(data_dict['names']),
+        'image/object/class/label': dataset_util.int64_list_feature(data_dict['labels']),
     }))
     return tf_example
 
@@ -50,18 +59,19 @@ def parse_pascal_xml(fname, labelmap):
 
     data_dict = {}
     root = ElementTree.parse(fname).getroot()
-    data_dict['image'] =  root.find('path').text
+    data_dict['image'] =  root.find('filename').text
     data_dict['width'] = int(root.find('size').find('width').text)
     data_dict['height'] = int(root.find('size').find('height').text)
     data_dict['depth'] = int(root.find('size').find('depth').text)
 
     data_dict['names'] = []
+    data_dict['labels'] = []
     data_dict['xmins'] = []
     data_dict['ymins'] = []
     data_dict['xmaxs'] = []
     data_dict['ymaxs'] = []
     for obj in root.findall('object'):
-        data_dict['names'].append(obj.find('name').text)
+        data_dict['names'].append(obj.find('name').text.encode('utf8'))
         data_dict['labels'].append(labelmap[obj.find('name').text])
         data_dict['xmins'].append(int(obj.find('bndbox').find('xmin').text))
         data_dict['ymins'].append(int(obj.find('bndbox').find('ymin').text))
@@ -71,14 +81,18 @@ def parse_pascal_xml(fname, labelmap):
     return(data_dict)
     
 
-def main(path, labelmap):
+def main():
     labelmap = {'mexpr' : 1}
-    for fname in glob.iglob(path + '/*.xml'):
-        print(fname)
-        print(parse_pascal_xml(fname, labelmap))
+    writer = tf.io.TFRecordWriter(output_path)
+    for fname in glob.iglob(os.path.join(annotations_dir, '*.xml')):
+        print("adding", fname)
+        tfr = create_tfrecord(parse_pascal_xml(fname, labelmap))
+        writer.write(tfr.SerializeToString())
+
+    writer.close()
         
 
 
 
 if __name__ == '__main__':
-    main('prepare_data')
+    main()
